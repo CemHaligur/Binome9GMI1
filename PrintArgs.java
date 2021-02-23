a8eb8018253cfc474dfd56582d9edf2b329fdc52
Selena-code <79517943+Selena-code@users.noreply.github.com>
2021-02-23 15:34:59 +0100

    Add files via upload

--- PrintArgs.java
+++ PrintArgs.java
@@ -3,9 +3,13 @@ public class PrintArgs {
 
 	public static void main(String[] args) {
 			// System.out.println(args[0]);
-			
+			// il n'affiche rien car on n'a rien passé en argument 
 			for (int i=0; i<args.length; i++)
 				System.out.println( (i+1)+" : "+ args[i]);
 		 	
+			for(String name : args)
+				System.out.println(name.toString());
+			
+			
 	}
 }
