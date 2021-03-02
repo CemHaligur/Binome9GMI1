6389083a327f07faacb413bea6d702fbfb5a0b61
Selena-code <79517943+Selena-code@users.noreply.github.com>
2021-03-02 14:17:18 +0100

    Add files via upload

---
+++ TD 4/Figure.java
@@ -0,0 +1,57 @@
+public class Figure {
+
+    private String Nom;
+    private String Couleur;
+    private float perimetre;
+    private float surface;
+
+    // Constructeur
+
+    public void Figure(String Nom, String Couleur)
+    {
+        this.Nom = Nom;
+        this.Couleur = Couleur;
+    }
+
+    // Accensseurs
+
+    public String getNom(){
+
+        return this.Nom;
+    }
+
+    public String getCouleur(){
+
+        return this.Couleur;
+    }
+
+    // Mutateurs
+
+    public void setNom(String perimetre){
+
+        this.Nom = Nom;
+    }
+
+    public void setCouleur(String surface){
+
+        this.Couleur = Couleur;
+    }
+
+
+    // Autres Methodes
+
+    public String toString() {
+
+        return "{" + this.getNom() + "," + this.getCouleur() + "}";
+
+    }
+
+    public boolean equals(Figure A){
+
+        return  ((this.getNom() == A.getNom())
+                && (this.getCouleur() == A.getCouleur()));
+
+    }
+
+
+}
