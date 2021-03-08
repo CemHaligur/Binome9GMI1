7862251d707317b8f5d159d504eec9ce6803bfa0
Cem Haligur <62490561+CemHaligur@users.noreply.github.com>
2021-03-08 12:53:31 +0100

    Update Main.java

--- TP 4  ZOO/Main.java
+++ TP 4  ZOO/Main.java
@@ -3,12 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         Zoo a1 = new Zoo("EISTI");
-        a1.ajouteAnimal(new Lion("fou", 15 , 16));
-        a1.ajouteAnimal(new Lion("fou1", 16, 17));
-        a1.ajouteAnimal(new Lion("fou2", 15, 18));
-        a1.ajouteAnimal(new Tigre("fou3", 15 , "Rouge"));
-        a1.ajouteAnimal(new Tigre("fou4", 15 , "Bleu"));
-        a1.ajouteAnimal(new Tigre("fou5", 15 , "Vert"));
+        a1.ajouteAnimal(new Lion("Selena", 15 , 16));
+        a1.ajouteAnimal(new Lion("Selena1", 16, 17));
+        a1.ajouteAnimal(new Lion("Selena2", 15, 18));
+        a1.ajouteAnimal(new Tigre("Selena3", 15 , "Rouge"));
+        a1.ajouteAnimal(new Tigre("Selena4", 15 , "Bleu"));
+        a1.ajouteAnimal(new Tigre("Selena5", 15 , "Vert"));
         a1.crier();
         a1.nombreNourriture();
 
